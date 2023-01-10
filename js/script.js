8ce5e8adce6d99148c736afee9bf0ba99d1f7f0f
rwdweb <rwd2186@gmail.com>
2023-01-10 12:10:20 +0600

    done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.banner-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots: true,
+        prevArrow:false,
+        nextArrow:false,
+        pauseOnHover:false,
+    });
+})
